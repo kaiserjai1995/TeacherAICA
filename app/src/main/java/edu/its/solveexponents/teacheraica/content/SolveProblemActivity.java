c4246dd0d125645b7bd5e98ec5c8eebfa4b02885
kaiserjai1995 <jai.belza95@gmail.com>
2016-12-08 22:43:32 +0800

    Enhance UI of Solve Exponents Section

--- app/src/main/java/edu/its/solveexponents/teacheraica/content/SolveProblemActivity.java
+++ app/src/main/java/edu/its/solveexponents/teacheraica/content/SolveProblemActivity.java
@@ -3,19 +3,13 @@ package edu.its.solveexponents.teacheraica.content;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.yarolegovich.lovelydialog.LovelyCustomDialog;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.its.solveexponents.teacheraica.R;
-import edu.its.solveexponents.teacheraica.adapter.SolveProblemRVAdapter;
-import edu.its.solveexponents.teacheraica.model.SolveProblemItems;
 
 /**
  * Created by jairus on 12/8/16.
@@ -25,39 +19,17 @@ public class SolveProblemActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
-    private List<SolveProblemItems> solve_problem_items;
-    private RecyclerView rv_solve_problem;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solve_problem);
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
         // Set a Toolbar to replace the ActionBar.
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        rv_solve_problem = (RecyclerView) findViewById(R.id.rv_solve_problem);
-
-        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
-        rv_solve_problem.setLayoutManager(llm);
-        rv_solve_problem.setHasFixedSize(true);
-
-        initializeData();
-        initializeAdapter();
-
-    }
-
-    public void initializeData(){
-        solve_problem_items = new ArrayList<>();
-        solve_problem_items.add(new SolveProblemItems(getString(R.string.solve_problem_problem_title),
-                getString(R.string.solve_problem_equation),
-                R.drawable.question));
-    }
-
-    public void initializeAdapter(){
-        SolveProblemRVAdapter adapter = new SolveProblemRVAdapter(getApplicationContext(), solve_problem_items);
-        rv_solve_problem.setAdapter(adapter);
     }
 
     @Override
