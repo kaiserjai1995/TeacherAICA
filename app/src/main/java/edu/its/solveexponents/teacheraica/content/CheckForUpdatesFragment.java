227d0963f66475d6341f98f5dac646f624b5aa03
kaiserjai1995 <jai.belza95@gmail.com>
2017-02-11 12:08:19 +0800

    Update Assets

--- app/src/main/java/edu/its/solveexponents/teacheraica/content/CheckForUpdatesFragment.java
+++ app/src/main/java/edu/its/solveexponents/teacheraica/content/CheckForUpdatesFragment.java
@@ -1,13 +1,17 @@
 package edu.its.solveexponents.teacheraica.content;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import edu.its.solveexponents.teacheraica.R;
+import mehdi.sakout.fancybuttons.FancyButton;
 
 /**
  * Created by jairus on 7/6/16.
@@ -28,6 +32,23 @@ public class CheckForUpdatesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_check_for_updates, container, false);
+
+        FancyButton check_for_updates_btn = (FancyButton) rootView.findViewById(R.id.check_for_updates_btn);
+
+        check_for_updates_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent viewIntent =
+                            new Intent("android.intent.action.VIEW",
+                                    Uri.parse("https://play.google.com/store/apps/details?id=edu.its.solveexponents.teacheraica"));
+                    startActivity(viewIntent);
+                }catch(Exception e) {
+                    Toast.makeText(getContext(),"Unable to Connect Try Again...", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
 
         // Inflate the layout for this fragment
         return rootView;
